66ea8a1c0c6cafeeaf3305777b42436ee8248988
saad154 <saadahmed0021@gmail.com>
2026-01-16 14:37:25 +0500

    walking enemy

---
+++ Assets/Scripts/Enemy/Enemy_AI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Enemy_AI : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float speed = 5f;
+    public float waitTime = 2f;
+
+    int index;
+    float waitTimer;
+
+    private void Update()
+    {
+        
+    }
+
+    public void Patrol()
+    {
+
+    }
+}
