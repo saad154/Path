7dbada07b5a740aa3355642b5387afb423968fbe
saad154 <saadahmed0021@gmail.com>
2026-01-18 10:12:24 +0500

    enemy

--- Assets/Scripts/Enemy/Enemy_AI.cs
+++ Assets/Scripts/Enemy/Enemy_AI.cs
@@ -3,19 +3,54 @@ using UnityEngine;
 public class Enemy_AI : MonoBehaviour
 {
     public Transform[] patrolPoints;
-    public float speed = 5f;
-    public float waitTime = 2f;
+    public Transform player;
+    public float speed = 2f;
+    public float stopDistance = 2f;
 
     int index;
-    float waitTimer;
+    Rigidbody rb;
 
-    private void Update()
+    void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    public void Patrol()
+    void FixedUpdate()
     {
+        if (ShouldStopPatrol())
+        {
+            StopPatrol();
+            return;
+        }
 
+        Patrol();
+    }
+
+    bool ShouldStopPatrol()
+    {
+        return Vector3.Distance(rb.position, player.position) < stopDistance;
+    }
+
+    void Patrol()
+    {
+        Vector3 target = patrolPoints[index].position;
+        target.y = rb.position.y;
+        target.z = rb.position.z;
+
+        Vector3 newPos = Vector3.MoveTowards(
+            rb.position,
+            target,
+            speed * Time.fixedDeltaTime
+        );
+
+        rb.MovePosition(newPos);
+
+        if (Vector3.Distance(rb.position, target) < 0.05f)
+            index = (index + 1) % patrolPoints.Length;
+    }
+
+    void StopPatrol()
+    {
+        rb.linearVelocity = Vector3.zero;
     }
 }
